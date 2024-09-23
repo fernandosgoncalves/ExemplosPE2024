9f777f786031892da1da707126f264b7484eaee3
fernandosgoncalves <fernandosilgon@gmail.com>
2024-09-23 15:50:47 -0300

    Atualizando repositorio

---
+++ ExemploEstruturas/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    typedef float REAL;
+
+    typedef struct {
+        int dia;
+        int mes;
+        int ano;
+    } DATA;
+
+    typedef struct {
+        char nome[30];
+        unsigned long int matricula;
+        char telefone[15];
+        DATA dataNasc;
+    } ALUNO;
+
+    REAL x = 33.22;
+
+    printf("%.2f", x);
+    //struct Aluno lista[5];
+    ALUNO lista[5] = {{"Pedro", 20243232, "123456", {10,10,2024}}};
+
+    strcpy(lista[1].nome, "Luiz");
+    lista[1].matricula = 2024032;
+    strcpy(lista[1].telefone, "48-95558-3333");
+
+    printf("Nome: %s\n", lista[0].nome);
+    printf("Telefone: %s\n", lista[0].telefone);
+    printf("Matricula: %d\n", lista[0].matricula);
+    printf("Data Nasc.: %d / %d / %d\n", lista[0].dataNasc.dia,lista[0].dataNasc.mes,lista[0].dataNasc.ano);
+
+    printf("Nome: %s\n", lista[1].nome);
+    printf("Telefone: %s\n", lista[1].telefone);
+    printf("Matricula: %d\n", lista[1].matricula);
+
+    //printf("%s %d %s", lista[0]);
+    return 0;
+}
